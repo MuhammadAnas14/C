5c9b6c76ec6a8f6b999174b39436193191308968
MuhammadAnas14 <anasm9877@gmail.com>
2021-11-26 00:00:03 +0500

    ddd

---
+++ IPC using shared memory/writer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+#define MAX 100
+  
+int main()
+{
+    key_t key = ftok("shmfile",65);
+  
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+  
+    char *str = (char*) shmat(shmid,(void*)0,0);
+  
+    cout<<"Write Data : ";
+    fgets(str,MAX,stdin);
+  
+    printf("Data written in memory: %s\n",str);
+      
+    shmdt(str);
+  
+    return 0;
+}
